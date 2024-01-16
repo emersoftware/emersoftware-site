c39b728a8e6686c8ed565f3aeda39e6492cdf5e8
emersoftware <87254036+emersoftware@users.noreply.github.com>
2024-01-16 19:20:19 -0300

    Add new components and dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'mint': '#C1FFCF',
+			'night': '#0400B4',
+			'forest': '#00280E',
+			'tangerine': '#FF823C',
+			'white': '#FFFFFF',
+			'black': '#000000',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
