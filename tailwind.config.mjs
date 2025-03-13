80cc703bc2b35626dd47044ccae708e0c55712ca
emersoftware <87254036+emersoftware@users.noreply.github.com>
2025-03-13 01:26:42 -0300

    (feat) add animations and a parallax scroll effect

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
-      mint: "#C1FFCF",
-      night: "#0400B4",
-      forest: "#00280E",
-      tangerine: "#FF823C",
-      white: "#FFFFFF",
-      black: "#000000",
-      red: "#FF0000",
+      mint: '#C1FFCF',
+      night: '#0400B4',
+      forest: '#00280E',
+      tangerine: '#FF823C',
+      white: '#FFFFFF',
+      black: '#000000',
+      red: '#FF0000',
     },
     extend: {},
   },
